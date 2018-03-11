614fa33d266e17ac0130a33e9a309674baeec81c
Joe Banks <joebanks10@gmail.com>
2018-03-11 16:43:16 -0700

    Render messages and message form

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,85 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    const io = window.io;
-    const socket = io();
+  constructor() {
+    super();
 
-    socket.on('connect', function () {
+    this.state = {
+      from: 'Joe',
+      text: '',
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    this.socket = window.io();
+
+    this.socket.on('connect', () => {
       console.log('Connected to server');
     });
 
-    socket.on('newMessage', function (msg) {
-      console.log(msg);
+    this.socket.on('newMessage', (msg) => {
+      console.log(this.state);
+
+      this.setState({
+        messages: this.state.messages.concat(msg)
+      });
     });
 
-    socket.on('disconnect', function () {
+    this.socket.on('disconnect', () => {
       console.log('Disconnected from server.');
+    });
+  }
+
+  onSubmit() {
+    const { from, text } = this.state;
+
+    this.socket.emit('createMessage', { 
+      from,
+      text
+    });
+
+    this.setState({
+      text: ''
+    })
+  }
+
+  onTextChange(e) {
+    this.setState({
+      text: e.target.value
     });
   }
 
   render() {
     return (
       <div className="App">
+      
         <header className="App-header">
           <h1 className="App-title">Chat App</h1>
         </header>
-        <p>Hello World</p>
+
+        <ol id="messages">
+          {this.state.messages.map((msg, i) => (
+            <li className="msg" key={`msg-${i}`}>
+              <span className="msg-from">{msg.from}:</span>&nbsp;
+              <span className="msg-text">{msg.text}</span>
+            </li>
+          ))}
+        </ol>
+
+        <form id="message-form" onSubmit={e => {
+          e.preventDefault();
+          this.onSubmit();
+        }}>
+          <input
+            type="text"
+            value={this.state.text}
+            placeholder="Message"
+            onChange={e => this.onTextChange(e)}
+          />
+          <button type="submit">Send</button>
+        </form>
+
       </div>
     );
   }
