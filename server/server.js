7d4b03747c88ed8979a6a147042b663c67bc81d9
Joe Banks <joebanks10@gmail.com>
2018-03-10 18:59:02 -0800

    Broadcast message when user connects

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New client connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat room'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined'
+  });
+
   socket.on('createMessage', function(msg) {
     io.emit('newMessage', {
       from: msg.from,
